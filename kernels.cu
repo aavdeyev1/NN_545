e91304ee510afcebba13b3fc9e8992a51241f35f
Gabe <ganderson5@ewu.edu>
2021-11-23 09:57:11 -0800

    Added Comments Outlines methods in main and kernel

---
+++ kernels.cu
@@ -0,0 +1,10 @@
+//This Kernel will be dedicated to forward feeding and backward propigation which gets error and new weights for one training pair.
+// The input will be...	void training(T trainData[64][numIn_],int trueOut[64][numOut_],const int numTrainSample,
+                      //const float learnRate,const long maxNumTrainIterate,float (*pLogisticFun)(float))
+
+//The output will be the updated weights for each training pair
+
+
+
+//This Kernel will be dedicated to updating the weights and biases for one batch
+
---
+++ main.cu
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include <limits>
+#include <string.h>
+
+#include <cpu.h>
+#include <kernels.h>
+
+// Make static array for these numbers so we can grow the number of hidden layers
+#define numIn_ 2
+#define numH_ 3
+#define numOut_ 1
+
+int main()
+{
+	int indata[8][8] = {
+							{ 1,1,1,1, 1,1,1,1},
+							{ 1,1,1,1, 1,1,1,1},
+							{ 1,1,0,1, 1,1,1,1},
+							{ 1,0,0,0, 1,1,1,1},
+							{ 1,0,0,0, 0,0,1,1},
+							{ 0,0,0,0, 0,0,1,1},
+							{ 0,0,0,0, 0,1,1,1},
+							{ 0,0,0,1, 1,1,1,1}
+						};
+	float fx(float);
+	int i,j,k = 0,input[64][2] = {0},output[64][1] = {0};
+
+	for(i = 0; i < 8; i ++)
+		for(j = 0; j< 8; j++)
+		{
+			input[k][0] = i;
+			input[k][1] = j;
+			output[k][0] = indata[i][j];
+			k ++;
+		}
+
+
+
+	bpNeuralNetwork<int> myBPNN;
+	myBPNN.training( input,output,64,0.02f,100000l,fx);
+	cout << "\n\n\n                Press any key to exit!";
+	getchar();
+	return 0;
+}
+
+
+// Make cudaMemcpy and cudaMalloc to allocate memory for gpu
+// Input will be the input and output arrays calculated in main numTrainSample, learnRate, long maxNumTrainIterate, *pLogisticFun
+void runGPU() {
+
+}
