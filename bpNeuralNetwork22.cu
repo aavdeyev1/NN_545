f117ced1537f446c82833a7084c3d0be7b1a00bf
bryanfischer <bfischer@zagmail.gonzaga.edu>
2021-12-01 21:45:15 -0800

    Moved everything to GPU kernel, using 1 thread. Output is not correct

--- bpNeuralNetwork22.cu
+++ bpNeuralNetwork22.cu
@@ -25,6 +25,105 @@
 
 using namespace std;
 
+__global__ void cuda_NN(int * trainData, 
+						int *trueOut,
+						const int numTrainSample,
+						const float learnRate,
+						const long maxNumTrainIterate,
+						int numNeuronIn_,
+						int *indata_,
+						int numNeuronHidden_,
+						float h,
+						float * wHidden_,
+						int* vHidden_,
+						int numNeuronOut_,
+						float * wOut_,
+						float* vOut_,
+						float * yError,
+						float * hError,
+						float *result){
+							
+	int y = 0;
+	h = 0;
+	for(int iterate = 1; iterate <= maxNumTrainIterate; iterate ++){
+		
+		for(int i = 0; i < numTrainSample; i++)
+			{
+				for(int k = 0; k < numNeuronIn_; k++)
+					//i is like row, k is like col
+					//calc format is: [row * numCols + col] is same as [row][col]
+					indata_[k] = trainData[i * 2 + k];
+
+				// forward computing
+				//
+				//
+				// compute vHidden
+				for(int m = 0; m < numNeuronHidden_; m++) 
+				{
+					for(int k = 0; k < numNeuronIn_; k++){
+						h = h + indata_[k] * wHidden_[m * 3 + (k + 1)];
+					}
+
+					h = h + wHidden_[m * 3];
+					vHidden_[m] = (float)(1.0f / (1 + exp(h * (-1))));
+
+					h = 0;
+				}
+			
+				// compute vOut
+				for(int m = 0; m < numNeuronOut_; m++)
+				{
+					for(int k = 0; k < numNeuronHidden_; k++)
+						y = y + vHidden_[k] * wOut_[m * 4 + (k + 1)];
+					y = y + wOut_[m * 4];
+					vOut_[m] = (float)(1.0f / (1 + exp(h * (-1))));
+
+					y = 0;
+				}
+
+				//
+				//
+				//backward compute
+			
+				//compute yError
+				for(int m = 0; m < numNeuronOut_; m++)
+					yError[m] =  vOut_[m] * ( 1 - vOut_[m]) * (  vOut_[m] - trueOut[i * 64 + m] );
+				
+				//compute hError
+				for(int m = 0; m < numNeuronHidden_; m++)
+				{
+					float temp = 0;
+					for(int k = 0; k < numNeuronOut_; k ++)
+						temp = temp + wOut_[k * 4 + (m + 1)] * yError[k];
+					hError[m] = temp * vHidden_[m] * (1 - vHidden_[m]);
+
+				}
+
+				//Adjust wOut[i][0] and wOut[i][j] and wHidden_
+				for(int m = 0; m < numNeuronOut_; m++)
+					wOut_[m * 4] = wOut_[m * 4] - learnRate * yError[m];
+
+				for(int m = 0; m < numNeuronOut_; m++)
+					for(int k = 0; k < numNeuronHidden_; k++)
+						wOut_[m * 4 + (k + 1)] = wOut_[m * 4 + (k + 1)] - learnRate * yError[m] * vHidden_[k];
+
+				for(int m = 0; m < numNeuronHidden_; m++)
+				{
+					wHidden_[m * 3] = wHidden_[m * 3] - learnRate * hError[m];
+					for(int k = 0; k < numNeuronIn_; k++)
+						wHidden_[m * 3 + (k + 1)] = wHidden_[m * 3 + (k + 1)] - learnRate * hError[m] * indata_[k];
+				}
+				
+				//one statement below did not consider the general neural network constructure, just for this assignment
+				//result[static_cast<int>(indata_[0])][static_cast<int>(indata_[1])] = vOut_[0];
+				result[static_cast<int>(indata_[0]) * 8 + static_cast<int>(indata_[1])] = vOut_[0];
+			/*	
+			*/
+			}// end for all samples
+	}	
+
+}
+
 // template function darray_new and darray_free to allocate memory dynamically
 template<class T> T** darray_new(T unit, int row, int col)
 {
@@ -135,10 +234,10 @@ template <class T> class bpNeuralNetwork
 		// pointer of pLogisticFun, is a function pointer, that enable us to use other logistic function in training conveniently
 		// number of rows of trueOut is equal to trainData's row number;One trueOut row corresponds to one trainData row. 
 		long iterate = 0L;
-		int i,k,m;
+		int k,m;
 		float h = 0;
-		float y = 0;
-		float temp = 0;
+		//float y = 0;
+		//float temp = 0;
 		float* yError = new float[numNeuronOut_];
 		float* hError = new float[numNeuronHidden_];
 		int numE = 0;
@@ -146,25 +245,193 @@ template <class T> class bpNeuralNetwork
 
 		float mytrim(float);
 
-		float result[8][8] = {0};     //Exclusively for this Assignment.The temporary matrix to store result 
+		float result[64];     //Exclusively for this Assignment.The temporary matrix to store result 
                                       // converted into matrix format, in order to output more convinietly
 
 		//Initiate the bpNetwork
 
+		int* h_trainData;
+		h_trainData = (int *)malloc(64 * numIn_ * sizeof(int));
+/*
+		cout << endl << "print the trainData before linearizing" << endl;
+		for(int i = 0; i < 64; i++){
+			for(int j = 0; j < numIn_; j++){
+				cout << trainData[i][j] << " ";
+			}
+		}
+		cout << endl;
+*/
+		//linearize 2d arrays
+		//
+		//formula is:
+		//arr[(numCols * i) + j] = arr[i][j]
+		for(int i = 0; i < 64; i++){
+			for(int j = 0; j < numIn_; j++){
+				h_trainData[(numIn_ * i) + j] = trainData[i][j];
+			}
+		}
+
+		float* h_wHidden;
+		h_wHidden = (float *)malloc(9 * sizeof(float));
+
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				h_wHidden[(3 * i) + j] = wHidden_[i][j];
+			}
+		}
+
+		float* h_wOut;
+		h_wOut = (float *)malloc(numOut_ * (numH_ + 1) * sizeof(float));
+
+		for(int i = 0; i < numOut_; i++){
+			for(int j = 0; j < (numH_ + 1); j++){
+				h_wOut[((numH_ + 1) * i) + j] = wOut_[i][j];
+			}
+		}
+
+		float* h_trueOut; //64 rows 1 col
+		h_trueOut = (float *)malloc(64 * sizeof(float));
+
+		for(int i = 0; i < 64; i++){
+			for(int j = 0; j < 1; j++){
+				h_trueOut[(1 * i) + j] = trueOut[i][j];
+			}
+		}
+/*
+		cout << endl << "print the traindata after linearizing" << endl;
+		for(int i = 0; i < 64 * numIn_; i++){
+			
+				cout << h_trainData[i] << " ";
+			
+		}
+		cout << endl;
+*/
 		//setup cuda
 
-		//copy indata_ to GPU
-		//copy wHidden_ to GPU
+		int* d_indata;
+		int* d_vHidden;
+		int* d_trainData;
+		float* d_wHidden;
+		float* d_h;
+		float* d_vOut;
+		float* d_yError;
+		float* d_hError;
+		float* d_wOut;
+		float* d_result;
+		int* d_trueOut;
+
+		
+		
+		//setup block and grid size
+		int blockSize, gridSize;
+		blockSize = 1;
+		gridSize = 1;
+
+		//allocate memory on GPU
+		cudaMalloc(&d_indata, numIn_ * sizeof(int));
+		cudaMalloc(&d_trainData, 128 * sizeof(int));
+		cudaMalloc(&d_trueOut, 64 * sizeof(int));
+		//cudaMalloc(&d_wHidden, numH_ * sizeof(float));
+		cudaMalloc(&d_h, sizeof(float));
+		cudaMalloc(&d_vHidden, numH_ * sizeof(int));
+		cudaMalloc(&d_vOut, numOut_ * sizeof(float));
+		cudaMalloc(&d_yError, numNeuronOut_ * sizeof(float));
+		cudaMalloc(&d_hError, numNeuronHidden_ * sizeof(float));
+		cudaMalloc(&d_wOut, numOut_ * (numH_ + 1) * sizeof(float));
+		cudaMalloc(&d_wHidden, numH_ * (numIn_ + 1) * sizeof(float));
+		cudaMalloc(&d_result, 64 * sizeof(float));
+
+
+		cudaMemcpy(d_indata, indata_, numIn_ * sizeof(int), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_trainData, trainData, 128 * sizeof(int), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_trueOut, h_trueOut, 64 * sizeof(int), cudaMemcpyHostToDevice);
+		//cudaMemcpy(d_wHidden, wHidden_, numH_ * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_h, &h, sizeof(float), cudaMemcpyHostToDevice);	
+		cudaMemcpy(d_vHidden, vHidden_, numH_ * sizeof(int), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_vOut, vOut_, numOut_ * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_yError, yError, numNeuronOut_ * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_hError, hError, numNeuronHidden_ * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_wOut, h_wOut, numOut_ * (numH_ + 1) * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_wHidden, h_wHidden, numH_ * (numIn_ + 1) * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_result, result, 64 * sizeof(float), cudaMemcpyHostToDevice);
+/*
+		cuda_NN<<<gridSize, blockSize>>>(d_trainData, 
+											d_trueOut, 
+											numTrainSample, 
+											learnRate, 
+											maxNumTrainIterate, 
+											numNeuronIn_, 
+											d_indata, 
+											numNeuronHidden_, 
+											h, 
+											d_wHidden, 
+											d_vHidden, 
+											numNeuronOut_, 
+											d_wOut, 
+											d_vOut, 
+											d_yError, 
+											d_hError, 
+											d_result);
+
+		cudaMemcpy(result, d_result, 64 * sizeof(float), cudaMemcpyDeviceToHost);
+
+		cout << endl << "print the result data" << endl;
+		for(int i = 0; i < 64; i++){
+			
+				cout << result[i] << " ";
+			
+		}
+*/
+		/*
+		things i need to copy to GPU memory:
+		indata_		x
+		h			x
+		vHidden_	x
+		
+		vOut_		x
+		yError		x
+		hError		x
+		wOut_		x
+		wHidden_	x
+		result		x
+
+
+		*/
 
 		
 
+		//call cuda kernel
+		//cuda_NN<<<gridSize, blockSize>>>((int**)trainData, (int**)trueOut, numTrainSample, learnRate, maxNumTrainIterate, numNeuronIn_, d_indata, numNeuronHidden_, h, d_wHidden, d_vHidden, numNeuronOut_, d_wOut, d_vOut, d_yError, d_hError, d_result);
+
+		//cudaMemcpy(result, d_result, 64 * sizeof(float), cudaMemcpyDeviceToHost);
 		
-		for(iterate = 1; iterate <= maxNumTrainIterate; iterate ++)
-		{
+		//for(iterate = 1; iterate <= maxNumTrainIterate; iterate ++)
+		//{
+
+			
+			cuda_NN<<<gridSize, blockSize>>>(d_trainData, 
+											d_trueOut, 
+											numTrainSample, 
+											learnRate, 
+											maxNumTrainIterate, 
+											numNeuronIn_, 
+											d_indata, 
+											numNeuronHidden_, 
+											h, 
+											d_wHidden, 
+											d_vHidden, 
+											numNeuronOut_, 
+											d_wOut, 
+											d_vOut, 
+											d_yError, 
+											d_hError, 
+											d_result);
+			/* 
 			for(i = 0; i < numTrainSample; i++)
 			{
 				for(k = 0; k < numNeuronIn_; k++)
 					indata_[k] = trainData[i][k];
+				
 				
 				
 
@@ -179,8 +446,10 @@ template <class T> class bpNeuralNetwork
 				// compute vHidden
 				for(m = 0; m < numNeuronHidden_; m++) 
 				{
-					for(k = 0; k < numNeuronIn_; k++) 
+					for(k = 0; k < numNeuronIn_; k++){
 						h = h + indata_[k] * wHidden_[m][k + 1];
+					}
+
 					h = h + wHidden_[m][0];
 					vHidden_[m] = pLogisticFun(static_cast<float>(h));
 
@@ -235,16 +504,18 @@ template <class T> class bpNeuralNetwork
 				result[static_cast<int>(indata_[0])][static_cast<int>(indata_[1])] = vOut_[0];
 			
 			}// end for all samples
-
+ 
 			
 			//output
 			
 			if(iterate == 10 || iterate == 100 || iterate == 1000 || iterate == 10000 || iterate == 100000)
 			{
+				cudaMemcpy(result, d_result, 64 * sizeof(float), cudaMemcpyDeviceToHost);
 				cout << "\n\nOuput values after " << iterate << " iterations: \n";
 				for(m = 0; m < 8; m++)
 				{
 					for(k = 0; k < 8; k ++)
+					/*
 						if ( (int)(result[m][k] + 0.5) == trueOut[m * 8 + k][0])
 						{
 							cout << setw(width) << mytrim(result[m][k]) << "  ";
@@ -254,6 +525,8 @@ template <class T> class bpNeuralNetwork
 							cout << setw(width) << mytrim(result[m][k]) << "* ";
 							numE ++;
 						}
+						
+					cout << result[m][k] << " ";
 					cout << "\n";
 		
 				}
@@ -261,8 +534,43 @@ template <class T> class bpNeuralNetwork
 				numE = 0;
                 
 			} // 
+			*/
+			
 
-		} // end for iteration
+		//} // end for iteration
+
+		cudaMemcpy(result, d_result, 64 * sizeof(float), cudaMemcpyDeviceToHost);
+
+		
+
+		cout << "\n\nOuput values after " << iterate << " iterations: \n";
+				for(m = 0; m < 8; m++)
+				{
+					for(k = 0; k < 8; k ++)
+						if ( (int)(result[m * 8 + k] + 0.5) == trueOut[m * 8 + k][0])
+						{
+							cout << setw(width) << mytrim(result[m * 8 + k]) << "  ";
+						}
+						else
+						{
+							cout << setw(width) << mytrim(result[m * 8 + k]) << "* ";
+							numE ++;
+						}
+					cout << "\n";
+		
+				}
+				cout << "==> " << numE << "  errors";
+				numE = 0;
+
+		cudaFree(d_indata);
+		cudaFree(d_vHidden);
+		cudaFree( d_wHidden);
+		cudaFree(d_h);
+		cudaFree(d_vOut);
+		cudaFree(d_yError);
+		cudaFree(d_hError);
+		cudaFree( d_wOut);
+		cudaFree( d_result);
 		
 	}// end for training
 
