a8c048ca84244a92e4195bd16091b0583f89cf6a
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-12-03 02:48:18 -0800

    123

--- kernels.cu
+++ kernels.cu
@@ -84,8 +84,8 @@ __global__ void kernel( int *input, float *output, float *vHidden, float *wHidde
             // }
             // adding the bias weight w0
             // atomicAdd(&h[0], wHidden[k*cols*rows + i*cols + 0]);
-            h[idx] = h[idx] + wHidden[k*cols*rows + i*cols + 0];
-            vHidden[k*numLayers+i] = h[idx];
+            h[idx] = h[idx] + wHidden[k*cols*rows + i*rows + 0];
+            vHidden[k*rows+i] = h[idx];
             printf("%5.02f ", h[idx]);
             h[idx] = 0;
         }
